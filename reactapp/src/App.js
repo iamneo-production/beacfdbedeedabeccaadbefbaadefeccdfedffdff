4fe6e4174259271f104cd0637f31af457ed87e9d
Iamneo <techadmin@iamneo.ai>
2023-08-05 14:24:53 +0000

    Backup Commit

--- reactapp/src/App.js
+++ reactapp/src/App.js
@@ -1,37 +1,14 @@
-import React, { useState } from 'react';
-import CreateArea from './components/CreateArea';
-import Note from './components/Note';
-import Header from './components/Header';
-import Footer from './components/Footer';
+import logo from './logo.svg';
+import './App.css';
 
-const App = () => {
-  const [notes, setNotes] = useState([]);
-
-  const handleAddNote = (newNote) => {
-    setNotes((prevNotes) => [...prevNotes, newNote]);
-  };
-
-  const handleDeleteNote = (index) => {
-    setNotes((prevNotes) => prevNotes.filter((_, i) => i !== index));
-  };
-
+function App() {
   return (
-    <div>
-      <Header />
-      <CreateArea onAdd={handleAddNote} />
-      <div>
-        {notes.map((note, index) => (
-          <Note
-            key={index}
-            title={note.title}
-            description={note.description}
-            onDelete={() => handleDeleteNote(index)}
-          />
-        ))}
-      </div>
-      <Footer />
+    <div className="App">
+      <div id="title"></div>
+      <div id="desc"></div>
+      <button id="addNote">OK</button>
     </div>
   );
-};
+}
 
 export default App;
