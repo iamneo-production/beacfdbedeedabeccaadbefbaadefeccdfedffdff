776a508e5214c6ec3a1f58baa53be703a3407579
Iamneo <techadmin@iamneo.ai>
2023-08-05 13:38:42 +0000

    Backup Commit

--- reactapp/src/components/CreateArea.js
+++ reactapp/src/components/CreateArea.js
@@ -1,65 +1,32 @@
-//CreateArea.js
-// import React, { useState } from 'react';
-
-// const CreateArea = ({ onAdd }) => {
-//   const [title, setTitle] = useState('');
-//   const [description, setDescription] = useState('');
-
-//   const handleTitleChange = (e) => {
-//     setTitle(e.target.value);
-//   };
-
-//   const handleDescriptionChange = (e) => {
-//     setDescription(e.target.value);
-//   };
-
-//   const handleAdd = () => {
-//     onAdd({ title, description });
-//     setTitle('');
-//     setDescription('');
-//   };
-
-//   return (
-//     <div>
-//       <input type="text" id="title" placeholder="Title" value={title} onChange={handleTitleChange} />
-//       <textarea id="desc" placeholder="Take a note..." rows="3" value={description} onChange={handleDescriptionChange} />
-//       <button id="addNotes" onClick={handleAdd}>Add</button>
-//     </div>
-//   );
-// };
-
-// export default CreateArea;
-
-import React, { useState, useCallback } from 'react';
+import React, { useState, useCallback, memo } from 'react';
 import styles from "../App.css";
 
 const CreateArea = ({ onAdd }) => {
-  const [title, setTitle] = useState('');
-  const [description, setDescription] = useState('');
+const [title, setTitle] = useState('');
+const [description, setDescription] = useState('');
 
-  const handleTitleChange = useCallback((e) => {
-    setTitle(e.target.value);
-  }, []);
+const handleTitleChange = useCallback((e) => {
+setTitle(e.target.value);
+}, []);
 
-  const handleDescriptionChange = useCallback((e) => {
-    setDescription(e.target.value);
-  }, []);
+const handleDescriptionChange = useCallback((e) => {
+setDescription(e.target.value);
+}, []);
 
-  const handleAdd = useCallback(() => {
-    if (title && description) {
-      onAdd({ title, description });
-      setTitle('');
-      setDescription('');
-    }
-  }, [title, description, onAdd]);
+const handleAdd = useCallback(() => {
+if (title && description) {
+onAdd({ title, description });
+setTitle('');
+setDescription('');
+}
+}, [title, description, onAdd]);
 
-  return (
-    <div>
-      <input type="text" id="title" placeholder="Title" value={title} onChange={handleTitleChange} />
-      <textarea id="desc" placeholder="Take a note..." rows="3" value={description} onChange={handleDescriptionChange} />
-      <button id="addNotes" onClick={handleAdd}>Add</button>
-    </div>
-  );
+return (
+<div>
+<input type="text" id="title" placeholder="Title" value={title} onChange={handleTitleChange} />
+<textarea id="desc" placeholder="Take a note..." rows="3" value={description} onChange={handleDescriptionChange} />
+<button id="addNotes" onClick={handleAdd}>Add</button>
+</div>
+);
 };
-
-export default React.memo(CreateArea);
+export default memo(CreateArea);
