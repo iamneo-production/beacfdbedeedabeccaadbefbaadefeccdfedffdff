15dfb83aaf5625e80421cf33501b8d3ec12fc2c8
Iamneo <techadmin@iamneo.ai>
2023-08-05 07:28:57 +0000

    Backup Commit

--- reactapp/src/components/CreateArea.js
+++ reactapp/src/components/CreateArea.js
@@ -1,31 +1,40 @@
-//CreateArea.js
 import React, { useState } from 'react';
 
 const CreateArea = ({ onAdd }) => {
-  const [title, setTitle] = useState('');
-  const [description, setDescription] = useState('');
+const [title, setTitle] = useState('');
+const [description, setDescription] = useState('');
 
-  const handleTitleChange = (e) => {
-    setTitle(e.target.value);
-  };
+const handleTitleChange = (e) => {
+setTitle(e.target.value);
+};
 
-  const handleDescriptionChange = (e) => {
-    setDescription(e.target.value);
-  };
+const handleDescriptionChange = (e) => {
+setDescription(e.target.value);
+};
 
-  const handleAdd = () => {
-    onAdd({ title, description });
-    setTitle('');
-    setDescription('');
-  };
+const addNote = () => {
+const note = {
+title: title,
+description: description,
+};
 
-  return (
-    <div>
-      <input type="text" id="title" placeholder="Title" value={title} onChange={handleTitleChange} />
-      <textarea id="desc" placeholder="Take a note..." rows="3" value={description} onChange={handleDescriptionChange} />
-      <button id="addNotes" onClick={handleAdd}>Add</button>
-    </div>
-  );
+// Add the note to the page.
+// ...
+};
+
+const handleAdd = () => {
+addNote();
+setTitle('');
+setDescription('');
+};
+
+return (
+<div>
+<input type="text" id="title" placeholder="Title" value={title} onChange={handleTitleChange} />
+<textarea id="desc" placeholder="Take a note..." rows="3" value={description} onChange={handleDescriptionChange} />
+<button id="addNotes" onClick={handleAdd}>Add</button>
+</div>
+);
 };
 
 export default CreateArea;
---
+++ reactapp/src/components/Note.js
@@ -0,0 +1,14 @@
+//Note.js
+import React from 'react';
+
+const Note = ({ title, description, onDelete }) => {
+  return (
+    <div>
+      <h1>{title}</h1>
+      <p>{description}</p>
+      <button onClick={onDelete}>Delete</button>
+    </div>
+  );
+};
+
+export default Note;
